cf3337882bd4369b9e98abd0bcddec3fe67bbe82
kzhrk <info@kzhrk.com>
2024-04-19 13:33:06 +0900

    wip: 一覧ページ作成

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,33 @@
 const route = useRoute();
 const page = route.query.page || 1;
 const res = await useFetch(`/api/posts?page=${page}`);
+const data = res.data;
+
+function getFormatedDate(dateString: string) {
+  const date = new Date(dateString);
+  return `${date.getFullYear()}年${date.getMonth() + 1}月${date.getDay()}日`
+}
+
 </script>
 
 <template>
-  <p>{{ res.data }}</p>
-  <p>一覧ページ</p>
+  <section v-for="({ metadata, episodeNumber }, i) in data" :key="i">
+    <h1>
+      <a :href="`/episode/${episodeNumber}`">
+        {{ metadata.title }}
+      </a>
+    </h1>
+    <time :datetime="metadata.date">{{ getFormatedDate(metadata.date) }}</time>
+    <p>{{ metadata.description }}</p>
+    <ul class="flex gap-5">
+      <li v-for="(id, index) in metadata.actor_ids" :key="index">
+        <figure class="text-center">
+          <img class="w-12 rounded-full" :src="`/images/actors/${id}.jpg`" :alt="`${id} のイメージ`">
+          <figcaption class="mt-1 text-sm">{{ id }}</figcaption>
+        </figure>
+      </li>
+    </ul>
+  </section>
 </template>
 
 <style scoped>
--- server/api/posts/index.ts
+++ server/api/posts/index.ts
@@ -1,12 +1,21 @@
-import { readdirSync } from 'node:fs'
-
+import { readdirSync, readFileSync } from 'node:fs'
+import { parse } from "marked";
+import parseMD from "parse-md";
 
 export default defineEventHandler(async (event) => {
   const per = 20;
-  const numbers = Array(readdirSync('./posts').length).fill(0).map((_, i) => i + 1);
   const query = getQuery(event);
+  const numbers = Array(readdirSync('./posts').length).fill(0).map((_, i) => i + 1).reverse();
   const page = Number.parseInt(query.page as string, 10);
-  const res = numbers.splice((page - 1) * per, per);
-  console.log(res);
-  return res;
+  const pages = numbers.splice((page - 1) * per, per);
+
+  return pages.map(p => {
+    const path = `./posts/${p}.md`;
+    const file = readFileSync(path);
+    const { metadata } = parseMD(file.toString());
+    return { 
+      metadata,
+      episodeNumber: p
+    }
+  })
 })
