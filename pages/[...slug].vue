3f52269040aab598952c31333cf14d7b7a6cbceb
kzhrk <info@kzhrk.com>
2024-04-27 10:29:25 +0900

    add 404 page

---
+++ pages/[...slug].vue
@@ -0,0 +1,7 @@
+<template>
+	<figure>
+		<a href="https://github.com/SAWARATSUKI/ServiceLogos">
+			<img src="/images/404.webp" alt="404 NOT FOUND" class="text-center">
+		</a>
+	</figure>
+</template>
