a29a2031db376c03ff1b744ad9c2323b058e837b
kzhrk <info@kzhrk.com>
2024-04-28 09:10:49 +0900

    update style in 404 page

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,7 +1,7 @@
 <template>
-	<figure>
-		<a href="https://github.com/SAWARATSUKI/ServiceLogos">
-			<img src="/images/404.webp" alt="404 NOT FOUND" class="text-center">
+	<figure class="text-center">
+		<a href="https://github.com/SAWARATSUKI/ServiceLogos" class="block w-full sm:w-1/2 mx-auto">
+			<img src="/images/404.webp" width="320" height="180" alt="404 NOT FOUND" class="w-full">
 		</a>
 	</figure>
 </template>
